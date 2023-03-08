dc41fc10ff11fc1b00e7b7e24f471c8edbe73301
Jonah Carneskog <jonah.carneskog@adesso-sweden.se>
2023-03-08 13:07:32 +0100

    Udemy end of section 2

---
+++ api/Data/StoreContext.cs
@@ -0,0 +1,13 @@
+using api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
