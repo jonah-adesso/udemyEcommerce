9fbb21b4d0163cc7798f733138ca23abe2c541ad
Jonah Carneskog <jonah.carneskog@adesso-sweden.se>
2023-03-10 14:36:15 +0100

    End of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -5,6 +5,7 @@ import {
   ThemeProvider
 } from '@mui/material';
 import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import Catalog from '../../features/catalog/Catalog';
 import { Header } from './Header';
 
@@ -26,7 +27,7 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog />
+        <Outlet />
       </Container>
     </ThemeProvider>
   );
